7de57525f304280ff2d2624f4ac7e0f563112dd3
aquaczaz <aquaczaz@gmail.com>
2022-06-01 14:32:26 -0500

    automatically update variants list of pokemon as number is changed

--- team_editor.py
+++ team_editor.py
@@ -3,6 +3,7 @@
 This is the team editor script for OBS, so you can do it all self-contained
 """
 
+from asyncio.windows_events import NULL
 from glob import glob
 import json
 import obspython as obs
@@ -15,6 +16,10 @@ debug = False
 
 # The location for the JSON file
 json_file = ""
+
+my_settings = NULL
+
+
 
 # Team information
 team = {
@@ -50,7 +55,7 @@ team = {
         "shiny": False,
     },
 }
-
+    
 
 def script_description():
     """Sets up the description
@@ -62,6 +67,83 @@ def script_description():
     """
     return "OBSPokemonHUD - Team Editor.\nAlso by Tom."
 
+
+def script_update(settings):
+    """Updates the settings values
+
+    This is a built-in OBS function.
+
+    This runs whenever a setting is changed or updated for the script. It also
+    sets up and removes the timer.
+    """
+    global json_file
+    global team
+    global v1
+    global my_settings
+
+    my_settings = settings
+    
+    #variantUpdate(v1)
+    
+
+    # If the team json file isn't given, return out so nothing happens
+    if not obs.obs_data_get_string(settings, "json_file"):
+        if debug:
+            print("Conditional: Returning because no JSON file is given")
+        return
+
+    if json_file != obs.obs_data_get_string(settings, "json_file"):
+        # If debug is enabled, print out this bit of text
+        if debug:
+            print("Conditional: New JSON File")
+
+        json_file = obs.obs_data_get_string(settings, "json_file")
+
+        with open(obs.obs_data_get_string(settings, "json_file"), 'r') as file:
+            new_team_data = json.load(file)
+
+        obs.obs_data_set_int(settings, "team_member_dex_1", new_team_data['slot1']['dexnumber'])
+        obs.obs_data_set_int(settings, "team_member_dex_2", new_team_data['slot2']['dexnumber'])
+        obs.obs_data_set_int(settings, "team_member_dex_3", new_team_data['slot3']['dexnumber'])
+        obs.obs_data_set_int(settings, "team_member_dex_4", new_team_data['slot4']['dexnumber'])
+        obs.obs_data_set_int(settings, "team_member_dex_5", new_team_data['slot5']['dexnumber'])
+        obs.obs_data_set_int(settings, "team_member_dex_6", new_team_data['slot6']['dexnumber'])
+
+        obs.obs_data_set_bool(settings, "team_member_shiny_1", new_team_data['slot1']['shiny'])
+        obs.obs_data_set_bool(settings, "team_member_shiny_2", new_team_data['slot2']['shiny'])
+        obs.obs_data_set_bool(settings, "team_member_shiny_3", new_team_data['slot3']['shiny'])
+        obs.obs_data_set_bool(settings, "team_member_shiny_4", new_team_data['slot4']['shiny'])
+        obs.obs_data_set_bool(settings, "team_member_shiny_5", new_team_data['slot5']['shiny'])
+        obs.obs_data_set_bool(settings, "team_member_shiny_6", new_team_data['slot6']['shiny'])
+
+    # Update the dex numbers
+    team['slot1']['dexnumber'] = obs.obs_data_get_int(settings, "team_member_dex_1")
+    team['slot2']['dexnumber'] = obs.obs_data_get_int(settings, "team_member_dex_2")
+    team['slot3']['dexnumber'] = obs.obs_data_get_int(settings, "team_member_dex_3")
+    team['slot4']['dexnumber'] = obs.obs_data_get_int(settings, "team_member_dex_4")
+    team['slot5']['dexnumber'] = obs.obs_data_get_int(settings, "team_member_dex_5")
+    team['slot6']['dexnumber'] = obs.obs_data_get_int(settings, "team_member_dex_6")
+
+    # Update the variant
+    team["slot1"]['variant'] = obs.obs_data_get_string(settings, "variant_1")
+    team["slot2"]['variant'] = obs.obs_data_get_string(settings, "variant_2")
+    team["slot3"]['variant'] = obs.obs_data_get_string(settings, "variant_3")
+    team["slot4"]['variant'] = obs.obs_data_get_string(settings, "variant_4")
+    team["slot5"]['variant'] = obs.obs_data_get_string(settings, "variant_5")
+    team["slot6"]['variant'] = obs.obs_data_get_string(settings, "variant_6")
+
+    # Update their shiny-ness
+    team['slot1']['shiny'] = obs.obs_data_get_bool(settings, "team_member_shiny_1")
+    team['slot2']['shiny'] = obs.obs_data_get_bool(settings, "team_member_shiny_2")
+    team['slot3']['shiny'] = obs.obs_data_get_bool(settings, "team_member_shiny_3")
+    team['slot4']['shiny'] = obs.obs_data_get_bool(settings, "team_member_shiny_4")
+    team['slot5']['shiny'] = obs.obs_data_get_bool(settings, "team_member_shiny_5")
+    team['slot6']['shiny'] = obs.obs_data_get_bool(settings, "team_member_shiny_6")
+
+
+    # If debug is enabled, print out this bit of text
+    if debug:
+        print("Function: Script Update")
 
 def script_properties():
     """Sets up the properties section of the "Scripts" window.
@@ -78,6 +160,7 @@ def script_properties():
     global v1
     global button
     global properties
+    global my_settings
 
     # Declare the properties object for us to mess with
     properties = obs.obs_properties_create()
@@ -107,7 +190,7 @@ def script_properties():
 
     # ------------------------------------------------------
 
-    obs.obs_properties_add_int(
+    dex1 = obs.obs_properties_add_int(
         properties,  # The properties variable
         "team_member_dex_1",  # Setting identifier string
         "Member 1 (Dex No.)",  # display name
@@ -115,13 +198,14 @@ def script_properties():
         898,  # Ending number
         1,  # Increment by
     )
-    v1 = obs.obs_properties_add_list(
+    obs.obs_properties_add_list(
         properties,
         "variant_1",
         "Variant",
         obs.OBS_COMBO_TYPE_LIST,
         obs.OBS_COMBO_FORMAT_STRING,
     )
+
     obs.obs_properties_add_bool(
         properties,  # The properties variable
         "team_member_shiny_1",  # Setting identifier string
@@ -237,20 +321,17 @@ def script_properties():
         properties,  # The properties variable
         "save_button",  # Setting identifier string
         "Save",
-        save_button_clicked
+        script_save
     )
-
-    
-
-    obs.obs_property_button_clicked(v1, save_button_clicked(properties, v1))               
-
+              
+    obs.obs_property_set_modified_callback(dex1, variantUpdate)
+    obs.obs_properties_apply_settings(properties, my_settings)
 
     if debug:
         print("Function: Properties")
 
     # Finally, return the properties so they show up
     return properties
-
 
 
 def script_defaults(settings):
@@ -283,89 +364,21 @@ def script_defaults(settings):
         print("Function: Defaults")
 
 
-def script_update(settings):
-    """Updates the settings values
+def variantUpdate(props, property,settings):
+    print('here')
 
-    This is a built-in OBS function.
-
-    This runs whenever a setting is changed or updated for the script. It also
-    sets up and removes the timer.
-    """
-    global json_file
-    global team
-    
-    variantUpdate(v1)
-
-    # If the team json file isn't given, return out so nothing happens
-    if not obs.obs_data_get_string(settings, "json_file"):
-        if debug:
-            print("Conditional: Returning because no JSON file is given")
-        return
-
-    if json_file != obs.obs_data_get_string(settings, "json_file"):
-        # If debug is enabled, print out this bit of text
-        if debug:
-            print("Conditional: New JSON File")
-
-        json_file = obs.obs_data_get_string(settings, "json_file")
-
-        with open(obs.obs_data_get_string(settings, "json_file"), 'r') as file:
-            new_team_data = json.load(file)
-
-        obs.obs_data_set_int(settings, "team_member_dex_1", new_team_data['slot1']['dexnumber'])
-        obs.obs_data_set_int(settings, "team_member_dex_2", new_team_data['slot2']['dexnumber'])
-        obs.obs_data_set_int(settings, "team_member_dex_3", new_team_data['slot3']['dexnumber'])
-        obs.obs_data_set_int(settings, "team_member_dex_4", new_team_data['slot4']['dexnumber'])
-        obs.obs_data_set_int(settings, "team_member_dex_5", new_team_data['slot5']['dexnumber'])
-        obs.obs_data_set_int(settings, "team_member_dex_6", new_team_data['slot6']['dexnumber'])
-
-        obs.obs_data_set_bool(settings, "team_member_shiny_1", new_team_data['slot1']['shiny'])
-        obs.obs_data_set_bool(settings, "team_member_shiny_2", new_team_data['slot2']['shiny'])
-        obs.obs_data_set_bool(settings, "team_member_shiny_3", new_team_data['slot3']['shiny'])
-        obs.obs_data_set_bool(settings, "team_member_shiny_4", new_team_data['slot4']['shiny'])
-        obs.obs_data_set_bool(settings, "team_member_shiny_5", new_team_data['slot5']['shiny'])
-        obs.obs_data_set_bool(settings, "team_member_shiny_6", new_team_data['slot6']['shiny'])
-
-    # Update the dex numbers
-    team['slot1']['dexnumber'] = obs.obs_data_get_int(settings, "team_member_dex_1")
-    team['slot2']['dexnumber'] = obs.obs_data_get_int(settings, "team_member_dex_2")
-    team['slot3']['dexnumber'] = obs.obs_data_get_int(settings, "team_member_dex_3")
-    team['slot4']['dexnumber'] = obs.obs_data_get_int(settings, "team_member_dex_4")
-    team['slot5']['dexnumber'] = obs.obs_data_get_int(settings, "team_member_dex_5")
-    team['slot6']['dexnumber'] = obs.obs_data_get_int(settings, "team_member_dex_6")
-
-    # Update the variant
-    team["slot1"]['variant'] = obs.obs_data_get_string(settings, "variant_1")
-    team["slot2"]['variant'] = obs.obs_data_get_string(settings, "variant_2")
-    team["slot3"]['variant'] = obs.obs_data_get_string(settings, "variant_3")
-    team["slot4"]['variant'] = obs.obs_data_get_string(settings, "variant_4")
-    team["slot5"]['variant'] = obs.obs_data_get_string(settings, "variant_5")
-    team["slot6"]['variant'] = obs.obs_data_get_string(settings, "variant_6")
-
-    # Update their shiny-ness
-    team['slot1']['shiny'] = obs.obs_data_get_bool(settings, "team_member_shiny_1")
-    team['slot2']['shiny'] = obs.obs_data_get_bool(settings, "team_member_shiny_2")
-    team['slot3']['shiny'] = obs.obs_data_get_bool(settings, "team_member_shiny_3")
-    team['slot4']['shiny'] = obs.obs_data_get_bool(settings, "team_member_shiny_4")
-    team['slot5']['shiny'] = obs.obs_data_get_bool(settings, "team_member_shiny_5")
-    team['slot6']['shiny'] = obs.obs_data_get_bool(settings, "team_member_shiny_6")
-
-
-    # If debug is enabled, print out this bit of text
-    if debug:
-        print("Function: Script Update")
-
-def variantUpdate(p):
     with open(f"{script_path()}map_{team['map']}.json", 'r') as file:
         sprite_map = json.load(file)
-    obs.obs_property_enabled(p)
-    obs.obs_property_list_clear(p)
+    obs.obs_property_list_clear(obs.obs_properties_get(props, "variant_1"))
     for x in range(1,7):
             if team['slot'+str(x)]['dexnumber'] > 0:
                 for sprite_variant in sprite_map['sprites'][str(team['slot'+str(x)]['dexnumber'])]:
                     if x == 1:
-                        obs.obs_property_list_insert_string(p, x-1, sprite_variant, sprite_variant)
                         print(sprite_variant)
+                        obs.obs_property_list_add_string(obs.obs_properties_get(props, "variant_1"), sprite_variant, sprite_variant)
+
+    return True
+                        
 
 def save_button_clicked(prop, p):
     """Activates when the save button is pressed
@@ -379,7 +392,7 @@ def save_button_clicked(prop, p):
 
 
 
-def save_team():
+def script_save(properties, p):
     """Saves the team information in to the team.json file that has been given
 
     Returns:
@@ -401,3 +414,5 @@ def save_team():
         print("Function: save_team")
         print(f"JSON file: {json_file}")
         print(f"Team data: {json.dumps(team)}")
+
+    script_properties()
