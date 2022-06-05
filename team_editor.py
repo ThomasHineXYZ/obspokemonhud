8ef450d9eee90b667dd5a0635ca80ecfd195f0b7
aquaczaz <aquaczaz@gmail.com>
2022-06-05 18:38:06 -0500

    resolved save button issue

--- team_editor.py
+++ team_editor.py
@@ -260,7 +260,7 @@ def script_properties():
         properties,  # The properties variable
         "save_button",  # Setting identifier string
         "Save",
-        save_button()
+        save_button
     )
               
     # Anytime a pokemon number changes, update the variant lists
@@ -405,7 +405,7 @@ def variantUpdate(props, property, settings):
         return True
 
 
-def save_button():
+def save_button(properties, p):
     """Saves the team information in to the team.json file that has been given
 
     Returns:
